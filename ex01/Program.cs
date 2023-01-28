146f5e06bf36843cdab44f96b97edd7c27f4a488
AndreySh <shingaryovan@gmail.com>
2023-01-28 13:52:36 +1200

    найти кубы чисел от 1 до Н

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(Math.Pow(i,3));
+}
