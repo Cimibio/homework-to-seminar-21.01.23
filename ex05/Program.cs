44b5512dbcf4209503ea5088867ab5bece384cd6
AndreySh <shingaryovan@gmail.com>
2023-01-28 14:28:55 +1200

    вычисления произведения чисел от 1 до N

---
+++ ex05/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int mult = 1;
+for (int i = 1; i <= N; i++)
+{
+    mult = mult * i;
+}
+Console.WriteLine("Произведение числел от 1 до " + N + " = " + mult);
