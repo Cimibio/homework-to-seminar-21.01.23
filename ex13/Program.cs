9718fe8e435583efd5ca01684f2e1822074de877
AndreySh <shingaryovan@gmail.com>
2023-01-28 15:51:15 +1200

    задан массив вещественный чисел, найдена разница между максимальным и минимальным числом, использован метод Max и Min

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -2,6 +2,10 @@
 double[] array = new double[10];
 for (int i = 0; i <= 9; i++)
 {
-    array[i] = new Random().Next(0,10);
-    Console.Write(array[i] + ", ");
+    array[i] = new Random().NextDouble() * 10;
+    Console.WriteLine(array[i] + ", ");
 }
+Console.WriteLine();
+Console.WriteLine("Минимальное число массива: " + array.Min());
+Console.WriteLine("Максимальное число массива: " + array.Max());
+Console.WriteLine("Разница максимального и минимального числа: " + (array.Max() - array.Min()));
