6e00a2aeb5222df6ea8908437fc60cac391f1cb0
AndreySh <shingaryovan@gmail.com>
2023-01-28 14:36:12 +1200

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ ex06/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 2; i <= N; i = i +2)
+{
+    Console.WriteLine("куб числа " + i + " = " + Math.Pow(i,3));
+}
