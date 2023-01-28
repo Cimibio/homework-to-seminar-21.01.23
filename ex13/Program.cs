2f5923862be8508b389d49d3aa707a5a48a9124a
AndreySh <shingaryovan@gmail.com>
2023-01-28 15:31:18 +1200

    добавлено 13е задание

---
+++ ex13/Program.cs
@@ -0,0 +1,7 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+double[] array = new double[10];
+for (int i = 0; i <= 9; i++)
+{
+    array[i] = new Random().Next(0,10);
+    Console.Write(array[i] + ", ");
+}
